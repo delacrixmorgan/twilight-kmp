eb31034a2721549a6b25dcd3aad422d699f8a798
Morgan Koh <delacrixmorgan@pm.me>
2024-05-09 12:56:58 +0200

    Add ListView

--- composeApp/src/commonMain/kotlin/ui/component/TimeRegionListRow.kt
+++ composeApp/src/commonMain/kotlin/ui/component/TimeRegionListRow.kt
@@ -1,19 +1,11 @@
 package ui.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import data.model.TimeRegion
 import data.model.localTime
@@ -23,30 +15,20 @@ internal fun TimeRegionListRow(timeRegion: TimeRegion) {
     Box(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.surfaceContainerLow, shape = MaterialTheme.shapes.small)
-            .padding(16.dp),
+            .padding(8.dp),
     ) {
-        Row(Modifier.fillMaxWidth()) {
-            Column(Modifier.align(alignment = Alignment.CenterVertically), verticalArrangement = Arrangement.spacedBy(2.dp)) {
-                Text(
-                    text = timeRegion.city,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+        ListView(
+            label = {
+                ListViewColumnLabel(
+                    label = timeRegion.city,
+                    description = timeRegion.zone
                 )
-                Text(
-                    text = timeRegion.zone,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    style = MaterialTheme.typography.bodyMedium
+            },
+            endLabel = {
+                ListViewColumnLabel(
+                    label = timeRegion.localTime()
                 )
             }
-            Spacer(modifier = Modifier.weight(1F))
-            Text(
-                text = timeRegion.localTime(),
-                modifier = Modifier
-                    .padding(vertical = 16.dp)
-                    .align(alignment = Alignment.CenterVertically),
-                color = MaterialTheme.colorScheme.onSurface,
-                style = MaterialTheme.typography.labelLarge
-            )
-        }
+        )
     }
 }
