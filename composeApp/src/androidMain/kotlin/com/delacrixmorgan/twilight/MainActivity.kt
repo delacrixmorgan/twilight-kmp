d8dbc3ee35c35fb4ae78b48d3ef7c709aedd9946
Morgan Koh <delacrixmorgan@pm.me>
2024-07-22 22:57:22 +0200

    Fix insetController system bar theme bug

--- composeApp/src/androidMain/kotlin/com/delacrixmorgan/twilight/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/delacrixmorgan/twilight/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.delacrixmorgan.twilight
 
 import App
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,10 +13,13 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
@@ -41,6 +45,16 @@ class MainActivity : ComponentActivity(), KoinComponent {
                     App(insetModifier)
                 }
             }
+            val view = LocalView.current
+            SideEffect {
+                val window = (view.context as ComponentActivity).window
+                val isDarkTheme = when (theme.value) {
+                    TwilightTheme.System -> isSystemInDarkTheme()
+                    TwilightTheme.Light -> false
+                    TwilightTheme.Dark -> true
+                }
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDarkTheme
+            }
             val lifecycleOwner = LocalLifecycleOwner.current
             LaunchedEffect(LocalLifecycleOwner.current) {
                 lifecycleOwner.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
@@ -48,6 +62,11 @@ class MainActivity : ComponentActivity(), KoinComponent {
                 }
             }
         }
+    }
+
+    private fun isSystemInDarkTheme(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
 }
 
