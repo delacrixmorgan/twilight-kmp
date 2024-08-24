87a489674272e525ac05e73745901b32dc5f57d4
Morgan Koh <delacrixmorgan@pm.me>
2024-08-24 17:50:44 +0200

    Fix iOS Koin dependencies

--- composeApp/src/iosMain/kotlin/Koin.kt
+++ composeApp/src/iosMain/kotlin/Koin.kt
@@ -1,12 +1,14 @@
 import di.mapperModule
 import di.repositoryModule
 import di.serviceModule
+import di.viewModelModule
 import org.koin.core.context.startKoin
 
 fun initKoin() {
     startKoin {
         modules(
             platformModule(),
+            viewModelModule(),
             serviceModule(),
             repositoryModule(),
             mapperModule()
